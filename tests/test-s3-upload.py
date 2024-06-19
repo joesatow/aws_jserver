aa4308465179c5aa06ea45492cd339f82a33d3d9
joe <joseph.satow@gmail.com>
2024-06-19 23:57:42 +0000

    update

--- tests/test-s3-upload.py
+++ tests/test-s3-upload.py
@@ -1,4 +1,6 @@
-from aws_jserver import upload_object_to_s3
+from aws_jserver import upload_object_to_s3, upload_file_to_s3
 
-test_load = b'This is a test object to upload.'
-upload_object_to_s3(test_load, "chart-stamp", "test.txt")
+# test_load = b'This is a test object to upload.'
+# upload_object_to_s3(test_load, "chart-stamp", "test.txt")
+
+upload_file_to_s3("/home/jsat/.site-packages/aws_jserver/tests/output.xlsx", "robin-finished-xlsx", "output.xlsx")
